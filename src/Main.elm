acf1191f9c8eef4ab4cfc9e86524361a8f4242ea
Christopher Miller <miller1997@gmail.com>
2020-07-20 10:07:44 -0400

    Types setup

---
+++ src/Main.elm
@@ -0,0 +1,43 @@
+module Main exposing (..)
+
+import Html.Attributes exposing (value)
+
+
+type alias Position =
+    { x : Int
+    , y : Int
+    }
+
+
+type Value
+    = Red
+    | Green
+
+
+type alias Cell =
+    { pos : Position
+    , val : Maybe Value
+    }
+
+
+type alias Board =
+    List Cell
+
+
+type alias Row =
+    Int
+
+
+type alias Column =
+    Int
+
+
+type Player
+    = Ome Value
+    | Two Value
+
+
+type alias Model =
+    { turn : Player
+    , board : Board
+    }
